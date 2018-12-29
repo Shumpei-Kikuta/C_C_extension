ef094e996a87685f7705e9c805facb0bc21fecf6
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2018-12-29 10:11:38 +0900

    readable

--- cons_tax.c
+++ cons_tax.c
@@ -3,11 +3,9 @@
 int main(){
     int water = 198;
     int milk = 138;
-    int sum;
+    int sum, payment, charge;
     sum = water + 2 * milk;
-    int payment;
     payment = (int)(sum * 1.05);
-    int charge;
     charge = 1000 - payment;
     printf("You get %d yen\n", charge);
     
