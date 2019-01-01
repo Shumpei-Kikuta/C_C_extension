16bf7f9fb37eb7aadb99ef29e806ffee4248973b
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2019-01-01 11:43:55 +0900

    add bubble sort

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+// prototype declarelation
+void insertion_sort(int *lists, int N);
+void bubble_sort(int *lists, int N);
+void input_lists(int *lists, int N);
+void print_lists(int *lists, int N);
+
+
 // insertion sort
 void insertion_sort(int *lists, int N){
     for (int i = 1; i < N; i++){
@@ -14,6 +21,26 @@ void insertion_sort(int *lists, int N){
     }
 }
 
+void bubble_sort(int *lists, int N){
+    bool flag = true;
+    int score = 0;
+    while (flag){
+        flag = false;
+        for (int i = N - 1; i > 0; i--){
+            if (lists[i] < lists[i - 1]){
+                flag = true;
+                int tmp = lists[i];
+                lists[i] = lists[i - 1];
+                lists[i - 1] = tmp;
+                score += 1;
+            }
+        }
+    }
+    print_lists(lists, N);
+    cout << "\n";
+    cout << score << endl;
+}
+
 // input lists
 void input_lists(int *lists, int N){
     for (int i = 0; i < N; i++){
@@ -24,8 +51,8 @@ void input_lists(int *lists, int N){
 // print list and add space between each element
 void print_lists(int *lists, int N){
     for (int i = 0; i < N; i++){
+        if (i) cout << " ";
         cout << lists[i];
-        cout << " ";
     }
 }
 
@@ -35,9 +62,9 @@ int main(){
     int *lists;
     lists = new int[N];
     input_lists(lists, N);
-    insertion_sort(lists, N);
+    bubble_sort(lists, N);
 
-    print_lists(lists, N);
+    // print_lists(lists, N);
     delete[] lists;
     return 0;
 }
